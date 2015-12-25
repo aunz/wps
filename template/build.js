68e33730f43f5cf8b1ee3cb880598f0137ce84c8
aunz <aunz10@yahoo.com>
2015-12-25 22:56:19 +1300

    add defineplugin

--- template/build.js
+++ template/build.js
@@ -6,6 +6,10 @@ const webpack = require('webpack')
 let config = require('./webpack.config.js')
 
 config.plugins.push(
+  new webpack.DefinePlugin({
+		__DEV__ : false,
+		'process.env.NODE_ENV' : '"production"'
+	}),
 	new webpack.optimize.DedupePlugin(),
 	new webpack.optimize.AggressiveMergingPlugin(),
 	new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}, sourceMap: false})
